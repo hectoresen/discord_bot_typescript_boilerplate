7479784c7605109a7a5fdb70767c3a57e48ff425
Hector <inforeshector@gmail.com>
2024-09-04 18:50:39 +0200

    FEAT: add basic build config

--- src/client/interfaces/discord-extended.interface.ts
+++ src/client/interfaces/discord-extended.interface.ts
@@ -1,5 +1,4 @@
 import { Client } from 'discord.js';
-// @ts-expect-error: Enmap types are not available, and we are bypassing type checking for now.
 import Enmap from 'enmap';
 
 export default interface ExtendedClient extends Client {
